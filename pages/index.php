3b6bbe78f37f464e4d958f70d90d9f5183179f3c
Djason irvinton SYLVAINCE <djasoirvinton.sylvaince@esih.edu>
2015-08-02 05:27:08 -0400

    Ti peyizan Pam
    Application web version preview

---
+++ pages/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ti peyizan pam</title>
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/common.css" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700' rel='stylesheet' type='text/css' />
+		<script type="text/javascript" src="js/modernizr.custom.79639.js"></script> 
+	<head>
+	<body>
+			<section class="main">
+				<ul class="ch-grid">
+					<li>
+						<div class="ch-item ch-img-1">
+							<div class="ch-info">
+								<h3>FWI LAKAY</h3>
+								<p>fwi lakay<a href="#">info</a></p><!-- fruits.php-->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-2">
+							<div class="ch-info">
+								<h3>OTEL LAKAY</h3>
+								<p>otèl<a href="#">info</a></p><!-- hotel.php-->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-3">
+							<div class="ch-info">
+								<h3>RESTORAN LAKAY</h3>
+								<p>restoran lakay<a href="#">info</a></p><!--restaurant.php-->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-4">
+							<div class="ch-info">
+								<h3>LWE MACHIN</h3>
+								<p>kote pou lwe machin<a href="#">info</a></p><!--location.php-->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-5">
+							<div class="ch-info">
+								<h3>FE TOURIS</h3>
+								<p>vizite sit touristik yo<a href="#">info</a></p> <!--touriste.php -->
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="ch-item ch-img-6">
+							<div class="ch-info">
+								<h3>PLAJ</h3>
+								<p>plaj lakay<a href="#">info</a></p><!-- plage.php-->
+							</div>
+						</div>
+					</li>
+				</ul>
+			</section>
+        </div>
+    </body>
+</html>
